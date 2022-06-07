33b743e3c76b5a021f4a367926fcc2d025ed5a11
rakeshrajput999 <baajigarrakesh@gmail.com>
2022-06-07 09:36:04 +0530

    1june meeting

---
+++ recursionprob.js
@@ -0,0 +1,38 @@
+let obj =[
+    {id:1,name:"ajay",age:16},
+    {id:2,name:"rohan",age:18},
+    {id:3,name:"veer",age:23},
+    {id:4,name:"abhay",age:24},
+]
+
+// get array of person of having age moe the
+
+// console.log(Object.entries(obj))
+
+
+const searchTerm = 18
+let result = []
+
+function getEachItem(object) {
+  object.forEach(item => {
+    searchItem(item)
+  })
+  return [...new Set(result)]
+
+};
+
+function searchItem(item) {
+  Object.keys(item).forEach(key => {
+    if (typeof item[key] === "object") {
+      searchItem(item[key])
+    }
+    if (typeof item[key] === "string") {
+      let searchAsRegEx = new RegExp(searchTerm, "gi");
+      if (item[key].match(searchAsRegEx)) {
+        result.push(item.id)
+      }
+    }
+  })
+}
+
+console.log(getEachItem(obj))
